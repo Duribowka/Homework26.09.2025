7e90e1e897df91c418ae6f0003a2d703394d94d2
Duribowka <s.boronin08@gmail.com>
2025-09-26 12:34:18 +0000

    Changed names added some tasks

---
+++ task51.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+
+    FILE *f;
+
+    for (int i = 1; i<argc; i++){
+        if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0){
+            printf("Verbose enabled.\n");
+        }
+        else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0){
+            printf("Here is list of all commands:\n1. -v\n2. --verbose\n3. -h\n4. --help\n");
+        }
+        else{
+            f = fopen(argv[i], "r");
+            if (f == NULL){
+                printf("Error: could not open file '%s'...\n", argv[i]);
+            }
+            else {
+                printf("File '%s' opened succesfully!\n", argv[i]);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ task52.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+
+    int number;
+    char output[50];
+    int isnum;
+
+    for (int i = 1; i<argc; i++){
+
+        isnum = 0;
+        if (strcmp(argv[i], "-n") == 0){
+            if (i+1 <= argc){
+                for (int j = 0; argv[i+1][j] != 0; j++){ 
+                    if (argv[i+1][j] - '0' > 9 || argv[i+1][j] - '0' < 0){
+                        isnum = 1;
+                        }
+                }
+                if (isnum == 1){
+                    printf("Error: '-n' requires a number...\n");
+                }
+                else{
+                    number = atoi(argv[i+1]);
+                }
+            }
+            else{
+                printf("Error: '-n' requires a number...\n");
+            }
+            
+        }
+        else if (strcmp(argv[i], "-o") == 0){
+            if (argv[i+1] != NULL){
+                for (int j = 0; argv[i+1][j] != 0; j++){
+                    output[j] = argv[i+1][j];
+                }
+                printf("File '%s' is set as output file!\n", output);
+            }
+            else{
+                printf("Error: '-o' requires a filename...\n");
+            }
+        }
+        else if(strcmp(argv[i], "-v") == 0){
+            printf("Verbose enabled.\n");
+        }
+        else{
+            printf("Error: unknown command...\n");
+        }
+    }
+
+    printf("Number value is %d\nOutput file is '%s'\n", number, output);
+
+    return 0;
+}
